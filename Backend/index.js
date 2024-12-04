2f83c9808be49124f30e1f7efb6aa7b14db6f060
Kishan Kulkarni <124907215+Lars1156@users.noreply.github.com>
2024-12-04 10:38:35 +0530

    add the api module tol the frontend Components

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const connection = require('./connection');
+const routerAPI = express.Router();
 const app = express();
 
 
@@ -15,7 +16,7 @@ connection('').then(()=>{
 // Middleware
 app.use(express.json());
 app.use(bodyParser.json());
-
+app.use('/api', routerAPI)
 app.listen(4007 , ()=>{
     console.log("Server is  running on port 4007");
     
