b4d3b83d876da9fff1f5821c43d81c7f9d859d2b
Kishan Kulkarni <124907215+Lars1156@users.noreply.github.com>
2024-12-04 10:31:55 +0530

    desing the user Controller

---
+++ Backend/controller/userController.js
@@ -0,0 +1,73 @@
+const User = require('../model/user');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const secret = "Kishan@1156";
+
+const registerUser = async(req,res)=>{
+    const {userName , email, password , role} = req.body;
+    console.log("**User details" , req.body);
+    try {
+        //  Cheacking the All valid Filds will entered or not
+           if(!userName || !email || password || role){
+             return res.status(400).json({msg:"Please enter all fields"});
+           }
+           const existingEmail = await User.findOne({email});
+           if(existingEmail){
+             return res.status(400).json({msg:"Email already exist"});
+           }
+           const user = new User({
+              userName , email , password , role
+           });
+        //    creating the new User 
+           const addData = await User (user);
+           await addData.save();
+           return res.status(200).json({msg:"User Created Successfully"});
+    } catch (error) {
+        console.error('Error registering user:', error);
+        return res.status(500).json({ msg: 'Server error, user not added' });         
+    }
+};
+
+const loginUser = async(req,res)=>{
+    const {email , password} = req.body;
+    try {
+         const user = await User.findOne({email});
+         if(!user){
+            return res.status(400).json({msg:"Email not found"});
+         }
+         const isMatch = await bcrypt.compare(password,user.password);
+         if(!isMatch){
+            return res.status(400).json({msg:"Invalid Password"});
+         }
+         const token = jwt.sign({id:user._id , role:user.role} , secret , {expiresIn:"1h"});
+         return res.status(200).json({token, msg:"Login Successfull"});
+    } catch (error) {
+        
+    }
+};
+
+const getAllUser = async(req,res)=> {
+    try {
+        // Fetch all users from the database
+        const users = await User.find();
+        res.status(200).json({
+          success: true,
+          message: 'Users fetched successfully',
+          data: users,
+        });
+      } catch (error) {
+        console.error('Error fetching users:', error);
+        res.status(500).json({
+          success: false,
+          message: 'Error fetching users',
+          error: error.message,
+        });
+      }
+};
+
+
+module.exports = {
+    registerUser,
+    loginUser,
+    getAllUser
+}
