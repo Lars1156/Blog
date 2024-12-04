a832460a6a0d50af315a5fdc595548430364085c
Kishan Kulkarni <124907215+Lars1156@users.noreply.github.com>
2024-12-04 10:07:41 +0530

    creattun the user Schema

--- Backend/model/user.js
+++ Backend/model/user.js
@@ -2,5 +2,37 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
+   userName : {
+    type: String , 
+    required : true
+   },
+   email:{
+    type:String, 
+     required:true,
+     unique:true,   
+   },
+   password:{
+      type:String,
+      required:true,
+      unique:true
+   },
+   role:{
+      type:String,
+      required:true,
+      enum:['Admin', 'author', 'reader'],
+      default:'reader'
+   }
+});
 
-})
+userSchema.pre('save', async function(next){
+    const user = this;
+    if(!user.isModified('password')){
+        return next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(user.password, salt);
+    user.password = hash;
+    next();
+});
+
+module.exports = mongoose.model('User', userSchema);
