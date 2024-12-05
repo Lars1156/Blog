4e332dd1fa08ca51b19059c63f4b455210dd80ba
Kishan Kulkarni <124907215+Lars1156@users.noreply.github.com>
2024-12-05 15:46:33 +0530

    delete ssome Components

--- Backend/index.js
+++ Backend/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const{connection} = require('./connection');
 const routerAPI = require('./router/api');
+const cros = require('cors');
 const app = express();
 
 
@@ -13,10 +14,19 @@ connection('mongodb://localhost:27017/blog-system').then(()=>{
     console.log("Connection Faild to data base", error);
     
 })
+// Frontend to Backend Connection Using Cros
+crosOption = {
+    origin:'http://localhost:3000',
+    optionSuccessfulStatus: 200
+}
+
 // Middleware
 app.use(express.json());
 app.use(bodyParser.json());
+app.use(cros(crosOption));
 app.use('/api', routerAPI)
+
+
 app.listen(4007 , ()=>{
     console.log("Server is  running on port 4007");
     
